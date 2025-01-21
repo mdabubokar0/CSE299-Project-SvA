77789655c7f64b1eb52e453320a5d172ee75d9d7
Md Abu Bokar <mdabu0212@gmail.com>
2025-01-21 21:13:07 +0600

    Progress 1 : Project setup

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+    }
+);
+
+const PORT = process.env.PORT || 8081;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
+});
